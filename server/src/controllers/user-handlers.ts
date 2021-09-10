2dc2c7a6f58aeb2a4df147995eeafb9505692bb7
fronte-finem <fronte.finem+github@ya.ru>
2021-09-10 20:02:14 +0300

    refactor: extract shared chat handler

--- server/src/controllers/user-handlers.ts
+++ server/src/controllers/user-handlers.ts
@@ -1,5 +1,4 @@
 import { GameService } from '@server/services/game-service';
-import { ApiFailMessage } from '@server/api-fail-message';
 import { ApiServerEvents } from '@shared/api-types/api-events';
 import { PointingPokerServerSocket } from 'types/server-socket';
 
@@ -9,17 +8,4 @@ export const getDisconnectHandler =
       .to(game.room)
       .emit(ApiServerEvents.USER_DISCONNECTED, socket.id);
     game.deleteUser(socket.id);
-  };
-
-export const getPostMessageHandler =
-  (socket: PointingPokerServerSocket, game: GameService) =>
-  (message: string) => {
-    let failMessage = message ? null : ApiFailMessage.MESSAGE_EMPTY;
-    if (message.length > 150) failMessage = ApiFailMessage.MESSAGE_TO_LONG;
-    if (failMessage) {
-      socket.emit(ApiServerEvents.POST_MESSAGE_FAILED, failMessage);
-      return;
-    }
-    const chatMessage = game.addMessage(socket.id, message);
-    game.server.to(game.room).emit(ApiServerEvents.MESSAGE_POSTED, chatMessage);
   };
