fbe2675bca6c646d194f44f935094c896a69ab46
fronte-finem <fronte.finem+github@ya.ru>
2021-09-10 19:56:03 +0300

    refactor: split tests on stages

---
+++ server/src/controllers/dealer-init.test.ts
@@ -0,0 +1,63 @@
+import { createServer, Server } from 'http';
+import { connectClient, emit } from '@server/test-helpers';
+import { initSocketServer } from '@server/init-socket-server';
+import { ApiClientEvents } from '@shared/api-types/api-events';
+import { ApiFailMessage } from '@server/api-fail-message';
+import { InitDealer } from '@shared/api-types/init';
+import { DealerToJoin } from '@shared/api-types/user';
+import { PointingPokerServer } from 'types/server-socket';
+import { ApiResponse, ResponseStatus } from '@shared/api-types/api-events-maps';
+import { zip } from '@shared/utils/array';
+
+const PORT = 42002;
+let httpServer: Server | undefined;
+let ioServer: PointingPokerServer | undefined;
+
+beforeEach(() => {
+  httpServer = createServer();
+  ioServer = initSocketServer(httpServer);
+  httpServer.listen(PORT);
+});
+
+afterEach(() => {
+  ioServer?.close();
+  httpServer?.close();
+});
+
+const dealerData: DealerToJoin = Object.freeze({
+  firstName: 'Tom Bombadil',
+  gameTitle: 'The Lord of the Rings',
+});
+
+describe('Dealer init tests.', () => {
+  it('server should validate game creation', async () => {
+    const dealerSockets = await Promise.all(
+      Array(3).fill(PORT).map(connectClient)
+    );
+    const badDealerData1 = { ...dealerData, gameTitle: '' };
+    const badDealerData2 = { ...dealerData, firstName: '' };
+
+    const responses: ApiResponse<InitDealer>[] = await Promise.all(
+      zip([badDealerData1, badDealerData2, dealerData], dealerSockets).map(
+        ([data, socket]) => emit(ApiClientEvents.CREATE_GAME, data, socket)
+      )
+    );
+
+    expect(responses[0].status).toBe(ResponseStatus.FAIL);
+    expect(responses[1].status).toBe(ResponseStatus.FAIL);
+    expect(responses[2].status).toBe(ResponseStatus.OK);
+
+    expect(responses[0].failMessage).toBe(ApiFailMessage.GAME_NEED_TITLE);
+    expect(responses[1].failMessage).toBe(ApiFailMessage.USER_NEED_NAME);
+    expect(responses[2].failMessage).toBeUndefined();
+
+    expect(responses[0].data).toBeUndefined();
+    expect(responses[1].data).toBeUndefined();
+    expect(typeof responses[2].data).toBe('object');
+    expect(typeof responses[2].data?.gameId).toBe('string');
+    expect(responses[2].data?.gameTitle).toBe(dealerData.gameTitle);
+    expect(responses[2].data?.users[0].firstName).toBe(dealerData.firstName);
+
+    dealerSockets.map((socket) => socket.close());
+  });
+});
--- server/src/init-socket-server.test.ts
+++ server/src/init-socket-server.test.ts
@@ -1,45 +1,12 @@
-import { initSocketServer } from '@server/init-socket-server';
 import { createServer, Server } from 'http';
-import { io as ioClient } from 'socket.io-client';
-import { ApiClientEvents, ApiServerEvents } from '@shared/api-types/api-events';
-import { ApiFailMessage } from '@server/api-fail-message';
-import { InitDealer } from '@shared/api-types/init';
-import { DealerToJoin, Role, UserToJoin } from '@shared/api-types/user';
-import { PointingPokerClientSocket } from 'types/client-socket';
+import { connectClient, onEvent } from '@server/test-helpers';
+import { initSocketServer } from '@server/init-socket-server';
 import { PointingPokerServer } from 'types/server-socket';
+import { ApiServerEvents } from '@shared/api-types/api-events';
 
-const PORT = 43434;
+const PORT = 42001;
 let httpServer: Server | undefined;
 let ioServer: PointingPokerServer | undefined;
-
-const connectClient = (): Promise<PointingPokerClientSocket> => {
-  const clientSocket = ioClient(`http://localhost:${PORT}`, {
-    transports: ['websocket'],
-    forceNew: true,
-  });
-  // clientSocket.on('connect', () =>
-  //   console.log(`client connected, id: ${clientSocket.id}`)
-  // );
-  return new Promise<PointingPokerClientSocket>((resolve) =>
-    clientSocket.on('connect', () => resolve(clientSocket))
-  );
-};
-
-const onEvent = (
-  event: ApiServerEvents,
-  clientSocket: PointingPokerClientSocket
-): Promise<any> => {
-  return new Promise((resolve) => clientSocket.on(event, resolve));
-};
-
-const onAnyEvent = (clientSocket: PointingPokerClientSocket): Promise<any> => {
-  return new Promise((resolve) =>
-    clientSocket.onAny((...args) => {
-      // console.log(args);
-      resolve(args);
-    })
-  );
-};
 
 beforeEach(() => {
   httpServer = createServer();
@@ -52,108 +19,23 @@ afterEach(() => {
   httpServer?.close();
 });
 
-describe('Pointing Poker socket.io API', () => {
-  const dealerData: DealerToJoin = {
-    firstName: 'Tom Bombadil',
-    gameTitle: 'The Lord of the Rings',
-  };
-  const gamerData: UserToJoin = {
-    firstName: 'Gandalf the Grey',
-    role: Role.GAMER,
-  };
-
-  it('server should ask game title', async () => {
-    const clientSocket = await connectClient();
-    clientSocket.emit(ApiClientEvents.CREATE_GAME, {
-      ...dealerData,
-      gameTitle: '',
-    });
-    const response = await onEvent(
-      ApiServerEvents.CREATE_GAME_FAILED,
-      clientSocket
+describe('Connections tests', () => {
+  it('server should connect/disconnect clients', async () => {
+    const SOCKETS_NUM = 10;
+    const sockets = await Promise.all(
+      Array(SOCKETS_NUM).fill(PORT).map(connectClient)
     );
-    expect(response).toBe(ApiFailMessage.GAME_NEED_TITLE);
-    clientSocket.close();
-  });
+    const getConnected = () => sockets.filter((socket) => socket.connected);
 
-  it('server should ask dealer name', async () => {
-    const clientSocket = await connectClient();
-    clientSocket.emit(ApiClientEvents.CREATE_GAME, {
-      ...dealerData,
-      firstName: '',
-    });
-    const response = await onEvent(
-      ApiServerEvents.CREATE_GAME_FAILED,
-      clientSocket
-    );
-    expect(response).toBe(ApiFailMessage.USER_NEED_NAME);
-    clientSocket.close();
-  });
+    expect(getConnected()).toHaveLength(SOCKETS_NUM);
 
-  it('server should return init-dealer object', async () => {
-    const clientSocket = await connectClient();
-    clientSocket.emit(ApiClientEvents.CREATE_GAME, dealerData);
-    const response: InitDealer = await onEvent(
-      ApiServerEvents.GAME_CREATED,
-      clientSocket
-    );
-    expect(typeof response).toBe('object');
-    expect(response.gameTitle).toBe(dealerData.gameTitle);
-    expect(response.users[0].firstName).toBe(dealerData.firstName);
-    clientSocket.close();
-  });
-
-  it('server should reject join to game with wrong game-id', async () => {
-    const clientSocket = await connectClient();
-    clientSocket.emit(ApiClientEvents.JOIN_GAME, 'xxx');
-    const response = await onEvent(
-      ApiServerEvents.JOIN_GAME_FAILED,
-      clientSocket
-    );
-    expect(response).toBe(ApiFailMessage.GAME_NOT_EXIST);
-    clientSocket.close();
-  });
-
-  it('server should allow login if game-id correct', async () => {
-    const [user, dealer] = await Promise.all([
-      connectClient(),
-      connectClient(),
-    ]);
-    expect(user.connected).toBeTruthy();
-    expect(dealer.connected).toBeTruthy();
-    // console.log(` dealer: ${dealer.id}\n user: ${user.id}`);
-    const created = onEvent(ApiServerEvents.GAME_CREATED, dealer);
-    dealer.emit(ApiClientEvents.CREATE_GAME, dealerData);
-    const { gameId } = await created;
-    // console.log(`game id: ${gameId}`);
-    user.emit(ApiClientEvents.JOIN_GAME, gameId);
-    const [event] = await onAnyEvent(user);
-    expect(event).toBe(ApiServerEvents.READY_TO_ADD_USER);
-    user.close();
-    dealer.close();
-  });
-
-  it('server should login users', async () => {
-    const [user1, user2, dealer] = await Promise.all(
-      [0, 0, 0].map(connectClient)
+    ioServer?.disconnectSockets(true);
+    await Promise.all(
+      getConnected().map((socket) =>
+        onEvent(ApiServerEvents.DISCONNECT, socket)
+      )
     );
 
-    dealer.emit(ApiClientEvents.CREATE_GAME, dealerData);
-    const { gameId } = await onEvent(ApiServerEvents.GAME_CREATED, dealer);
-    expect(typeof gameId).toBe('string');
-
-    user1.emit(ApiClientEvents.JOIN_GAME, gameId);
-    user2.emit(ApiClientEvents.JOIN_GAME, gameId);
-    let [[ev1], [ev2]] = await Promise.all([user1, user2].map(onAnyEvent));
-    expect(ev1).toBe(ApiServerEvents.READY_TO_ADD_USER);
-    expect(ev2).toBe(ApiServerEvents.READY_TO_ADD_USER);
-
-    user1.emit(ApiClientEvents.ADD_USER, { ...gamerData, firstName: '' });
-    user2.emit(ApiClientEvents.ADD_USER, { ...gamerData });
-    [[ev1], [ev2]] = await Promise.all([user1, user2].map(onAnyEvent));
-    expect(ev1).toBe(ApiServerEvents.LOGIN_FAILED);
-    expect(ev2).toBe(ApiServerEvents.LOGGED_IN);
-
-    [user1, user2, dealer].map((socket) => socket.close());
+    expect(getConnected()).toHaveLength(0);
   });
 });
