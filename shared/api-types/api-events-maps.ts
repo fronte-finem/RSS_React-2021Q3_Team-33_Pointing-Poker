434fae8044fd4c02cb84f170c7aa9c556191ac2a
fronte-finem <fronte.finem+github@ya.ru>
2021-09-10 19:46:06 +0300

    refactor: change fail-events on ack-callback responses

--- shared/api-types/api-events-maps.ts
+++ shared/api-types/api-events-maps.ts
@@ -10,67 +10,116 @@ import { CardScore, GameSettings } from '@shared/api-types/game-settings';
 import { InitDealer, InitUser } from '@shared/api-types/init';
 import { ChatMessage, KickResult, KickVoteInit } from '@shared/api-types/chat';
 
-type NoPayloadHandler = () => void;
-type FailHandler = (description: string) => void;
-
 type ApiClientEventsMap = { [event in ApiClientEvents]: Function };
 type ApiServerEventsMap = { [event in ApiServerEvents]: Function };
 
+export const enum ResponseStatus {
+  FAIL = 'FAIL',
+  OK = 'OK',
+}
+export type ApiResponseFail = {
+  status: ResponseStatus.FAIL;
+  failMessage: string;
+  data: undefined;
+};
+export type ApiResponseOk<Payload> = {
+  status: ResponseStatus.OK;
+  failMessage: undefined;
+  data: Payload;
+};
+export type ApiResponse<Payload> = ApiResponseFail | ApiResponseOk<Payload>;
+export type AckCallback<Payload> = (response: ApiResponse<Payload>) => void;
+
+export const isOk = ({ status }: ApiResponse<any>) =>
+  status === ResponseStatus.OK;
+export const isFail = ({ status }: ApiResponse<any>) =>
+  status === ResponseStatus.FAIL;
+
+export function setFail(message: string): ApiResponseFail {
+  return { status: ResponseStatus.FAIL, failMessage: message, data: undefined };
+}
+export function setOk<Payload>(data: Payload): ApiResponseOk<Payload> {
+  return { status: ResponseStatus.OK, failMessage: undefined, data };
+}
+
 export interface PointingPokerClientToServerEvents extends ApiClientEventsMap {
-  [ApiClientEvents.CREATE_GAME]: (dealerToJoin: DealerToJoin) => void;
-  [ApiClientEvents.CANCEL_GAME]: NoPayloadHandler;
-  [ApiClientEvents.DISCONNECT]: NoPayloadHandler;
-  [ApiClientEvents.CHANGE_GAME_TITLE]: (title: string) => void;
-  [ApiClientEvents.JOIN_GAME]: (gameId: string) => void;
-  [ApiClientEvents.ADD_USER]: (user: UserToJoin) => void;
-  [ApiClientEvents.IS_USER_JOIN_ALLOWED]: (isAllowed: boolean) => void;
-  [ApiClientEvents.POST_MESSAGE]: (message: string) => void;
-  [ApiClientEvents.KICK_USER]: (userId: string) => void;
-  [ApiClientEvents.VOTE_TO_KICK_USER]: (kick: boolean) => void;
-  [ApiClientEvents.ADD_ISSUE]: (issue: IssueBase) => void;
-  [ApiClientEvents.DELETE_ISSUE]: (issueId: string) => void;
-  [ApiClientEvents.EDIT_ISSUE]: (issue: Issue) => void;
-  [ApiClientEvents.START_GAME]: (gameSettings: GameSettings) => void;
-  [ApiClientEvents.END_GAME]: NoPayloadHandler;
-  [ApiClientEvents.START_ROUND]: (issueId: string) => void;
-  [ApiClientEvents.END_ROUND]: NoPayloadHandler;
-  [ApiClientEvents.ADD_SCORE]: (score: CardScore) => void;
+  [ApiClientEvents.CREATE_GAME]: (
+    dealerToJoin: DealerToJoin,
+    ackCallback: AckCallback<InitDealer>
+  ) => void;
+  [ApiClientEvents.CANCEL_GAME]: () => void;
+  [ApiClientEvents.DISCONNECT]: () => void;
+  [ApiClientEvents.CHANGE_GAME_TITLE]: (
+    title: string,
+    ackCallback: AckCallback<string>
+  ) => void;
+  [ApiClientEvents.JOIN_GAME]: (
+    gameId: string,
+    ackCallback: AckCallback<string>
+  ) => void;
+  [ApiClientEvents.ADD_USER]: (
+    user: UserToJoin,
+    ackCallback: AckCallback<InitUser>
+  ) => void;
+  [ApiClientEvents.POST_MESSAGE]: (
+    message: string,
+    ackCallback: AckCallback<string>
+  ) => void;
+  [ApiClientEvents.KICK_USER]: (
+    userId: string,
+    ackCallback: AckCallback<string>
+  ) => void;
+  [ApiClientEvents.VOTE_TO_KICK_USER]: (
+    kick: boolean,
+    ackCallback: AckCallback<boolean>
+  ) => void;
+  [ApiClientEvents.ADD_ISSUE]: (
+    issue: IssueBase,
+    ackCallback: AckCallback<Issue>
+  ) => void;
+  [ApiClientEvents.DELETE_ISSUE]: (
+    issueId: string,
+    ackCallback: AckCallback<string>
+  ) => void;
+  [ApiClientEvents.EDIT_ISSUE]: (
+    issue: Issue,
+    ackCallback: AckCallback<Issue>
+  ) => void;
+  [ApiClientEvents.START_GAME]: (
+    gameSettings: GameSettings,
+    ackCallback: AckCallback<GameSettings>
+  ) => void;
+  [ApiClientEvents.END_GAME]: (ackCallback: AckCallback<true>) => void;
+  [ApiClientEvents.START_ROUND]: (
+    issueId: string,
+    ackCallback: AckCallback<string>
+  ) => void;
+  [ApiClientEvents.END_ROUND]: (ackCallback: AckCallback<true>) => void;
+  [ApiClientEvents.ADD_SCORE]: (
+    score: CardScore,
+    ackCallback: AckCallback<CardScore>
+  ) => void;
 }
 
 export interface PointingPokerServerToClientEvents extends ApiServerEventsMap {
-  [ApiServerEvents.CREATE_GAME_FAILED]: FailHandler;
-  [ApiServerEvents.GAME_CREATED]: (initDealer: InitDealer) => void;
-  [ApiServerEvents.GAME_CANCELED]: NoPayloadHandler;
-  [ApiServerEvents.CHANGE_GAME_TITLE_FAILED]: FailHandler;
   [ApiServerEvents.GAME_TITLE_CHANGED]: (title: string) => void;
-  [ApiServerEvents.JOIN_GAME_FAILED]: FailHandler;
-  [ApiServerEvents.READY_TO_ADD_USER]: NoPayloadHandler;
-  [ApiServerEvents.ALLOW_USER_JOIN]: (user: UserToJoin) => void;
-  [ApiServerEvents.LOGIN_FAILED]: FailHandler;
-  [ApiServerEvents.LOGGED_IN]: (initUser: InitUser) => void;
+  [ApiServerEvents.GAME_CANCELED]: () => void;
+  [ApiServerEvents.ALLOW_USER_JOIN]: (
+    user: UserToJoin,
+    ackCallback: (allow: boolean) => void
+  ) => void;
   [ApiServerEvents.USER_JOINED]: (user: User) => void;
   [ApiServerEvents.USER_DISCONNECTED]: (userId: string) => void;
-  [ApiServerEvents.POST_MESSAGE_FAILED]: FailHandler;
   [ApiServerEvents.MESSAGE_POSTED]: (chatMessage: ChatMessage) => void;
-  [ApiServerEvents.KICK_USER_FAILED]: FailHandler;
   [ApiServerEvents.KICK_VOTE_STARTED]: (kickVoteInit: KickVoteInit) => void;
-  [ApiServerEvents.VOTE_TO_KICK_USER_FAILED]: FailHandler;
   [ApiServerEvents.USER_KICK_RESULT]: (kickResult: KickResult) => void;
   [ApiServerEvents.KICKED]: (reason: string) => void;
-  [ApiServerEvents.ADD_ISSUE_FAILED]: FailHandler;
   [ApiServerEvents.ISSUE_ADDED]: (issue: Issue) => void;
-  [ApiServerEvents.DELETE_ISSUE_FAILED]: FailHandler;
   [ApiServerEvents.ISSUE_DELETED]: (issueId: string) => void;
-  [ApiServerEvents.EDIT_ISSUE_FAILED]: FailHandler;
   [ApiServerEvents.ISSUE_EDITED]: (issue: Issue) => void;
-  [ApiServerEvents.START_GAME_FAILED]: FailHandler;
   [ApiServerEvents.GAME_STARTED]: (gameSettings: GameSettings) => void;
-  [ApiServerEvents.END_GAME_FAILED]: FailHandler;
   [ApiServerEvents.GAME_ENDED]: (gameResult: GameResult) => void;
-  [ApiServerEvents.START_ROUND_FAILED]: FailHandler;
   [ApiServerEvents.ROUND_STARTED]: (issueId: string) => void;
-  [ApiServerEvents.END_ROUND_FAILED]: FailHandler;
   [ApiServerEvents.ROUND_ENDED]: (roundResult: RoundResult) => void;
-  [ApiServerEvents.ADD_SCORE_FAILED]: FailHandler;
   [ApiServerEvents.SCORE_ADDED]: (userId: string) => void;
 }
