dc2eec5d7c76286d68b452f030160928b3da5868
fronte-finem <fronte.finem+github@ya.ru>
2021-09-20 19:55:45 +0300

    refactor: extract styles

---
+++ client/src/components/pages/main-page/main-page.styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledMainPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px 26px;
+
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+`;
