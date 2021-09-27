4738e8a35335c1274db72aa880df60a604db705d
KononOleg <konon.oleg97@gmail.com>
2021-09-27 19:51:28 +0300

    refactor: decomposition game result

--- client/src/components/pages/game-result/components/game-results.tsx
+++ client/src/components/pages/game-result/components/game-results.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { IssueCard } from '@client/components/shared/issue/issue-card';
-import { GameCard } from '@client/components/shared/game-card/game-card';
-import {
-  StyleGameResultCard,
-  StyleGameResultIssue,
-  StyleGameResultsWrapper,
-  StyleGameResultText,
-} from '../game-result-styles';
-import { CardResults, GameResultsRender } from '../shared/types/types';
+
+import { StyleGameResultsWrapper } from '../game-result-styles';
+import { GameResultsRender } from '../shared/types/types';
+import { IssueStats } from './issue-stats';
 
 export const GameResults: React.FC<{
   gameResultsRender: GameResultsRender[];
@@ -18,20 +13,7 @@ export const GameResults: React.FC<{
   return (
     <StyleGameResultsWrapper>
       {gameResultsRender.map((result: GameResultsRender) => (
-        <div>
-          <IssueCard issue={result.issue} />
-          <StyleGameResultIssue>
-            {result.scores.map((score: CardResults) => (
-              <StyleGameResultCard>
-                <GameCard
-                  score={score.score as number}
-                  scoreType={scoreType || 'SP'}
-                />
-                <StyleGameResultText>{score.percent} %</StyleGameResultText>
-              </StyleGameResultCard>
-            ))}
-          </StyleGameResultIssue>
-        </div>
+        <IssueStats result={result} scoreType={scoreType} />
       ))}
     </StyleGameResultsWrapper>
   );
