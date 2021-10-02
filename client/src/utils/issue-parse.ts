ac3872ee5b1e6aeb3e93e3eb8f28bc426e3e1c92
fronte-finem <fronte.finem+github@ya.ru>
2021-10-02 18:39:16 +0300

    feat: add utils for parse file to issues

---
+++ client/src/utils/issue-parse.ts
@@ -0,0 +1,19 @@
+import XLSX from 'xlsx';
+import { IssueBase } from '@shared/api-types/issue';
+import { loadFile } from '@client/utils/file';
+import { filterIssues } from '@shared/api-validation/issue';
+
+export const parseFileToIssue = async (file: File): Promise<IssueBase[]> => {
+  const data = await loadFile(file);
+  if (data.length === 0) return [];
+  try {
+    const workbook = XLSX.read(data, { type: 'array' });
+    if (workbook.SheetNames.length === 0) return [];
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    const issues = XLSX.utils.sheet_to_json<IssueBase>(sheet);
+    return filterIssues(issues);
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
