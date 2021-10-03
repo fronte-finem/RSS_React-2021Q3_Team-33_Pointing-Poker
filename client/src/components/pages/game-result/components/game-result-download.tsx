2a5a1e4b260bfd1a799ba547d58dbed9db9bfe78
fronte-finem <fronte.finem+github@ya.ru>
2021-10-03 12:59:37 +0300

    fix: add splitting to production bundle chunks

--- client/src/components/pages/game-result/components/game-result-download.tsx
+++ client/src/components/pages/game-result/components/game-result-download.tsx
@@ -1,4 +1,3 @@
-import XLSX from 'xlsx';
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { useStateService } from '@client/providers/state-service';
@@ -8,12 +7,13 @@ import { getGameResultXLS } from '../shared/utils/utils';
 export const DownloadButton = observer(() => {
   const { gameState } = useStateService();
 
-  const downloadXLS = () => {
+  const downloadXLS = async () => {
+    const XLSX = await import('xlsx');
     const ws = XLSX.utils.json_to_sheet(
       getGameResultXLS(gameState.getStatistics())
     );
     const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'SheetJS');
+    XLSX.utils.book_append_sheet(wb, ws, gameState.title);
     XLSX.writeFile(wb, 'GameResult.xlsx');
   };
 
--- client/src/utils/issue-parse.ts
+++ client/src/utils/issue-parse.ts
@@ -1,4 +1,3 @@
-import XLSX from 'xlsx';
 import { IssueBase } from '@shared/api-types/issue';
 import { loadFile } from '@client/utils/file';
 import { filterIssues } from '@shared/api-validation/issue';
@@ -7,6 +6,7 @@ export const parseFileToIssue = async (file: File): Promise<IssueBase[]> => {
   const data = await loadFile(file);
   if (data.length === 0) return [];
   try {
+    const XLSX = await import('xlsx');
     const workbook = XLSX.read(data, { type: 'array' });
     if (workbook.SheetNames.length === 0) return [];
     const sheet = workbook.Sheets[workbook.SheetNames[0]];
